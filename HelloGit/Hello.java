778fe99486fc1cd9f2967e2a6b6d0b20232d3933
zoeybuthorne <zoeybuthorne@localhost>
2025-08-28 13:33:16 -0500

    Initial Commit
    .metadata
    bin/
    tmp/
    *.tmp
    *.bak
    *.swp
    *~.nib
    local.properties
    .settings/
    .loadpath
    .recommenders
    
    .externalToolBuilders/
    
    *.launch
    
    *.pydevproject
    
    .cproject
    
    .autotools
    
    .factorypath
    
    .buildpath
    
    .target
    
    .tern-project
    
    .texlipse
    
    .springBeans
    
    .recommenders/
    
    .apt_generated/
    .apt_generated_tests/
    
    .cache-main
    .scala_dependencies
    .worksheet

---
+++ HelloGit/Hello.java
@@ -0,0 +1,16 @@
+
+/**
+ * Author: Zoey Buthorne (zbuthorne@.unl.edu) 
+ * Date: 08/27/2025
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author zbuthorne
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Zoey Buthorne");
+	}
+}
---
+++ HelloGit/src/unl/soc/Hello.java
@@ -0,0 +1,17 @@
+package unl.soc;
+
+/**
+ * Author: Zoey Buthorne (zbuthorne@.unl.edu) 
+ * Date: 08/27/2025
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author zbuthorne
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Zoey Buthorne");
+	}
+}
